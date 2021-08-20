4395679a5ddf05a4cd0c52c1471629f294315f05
vincent770 <vt.y@qq.com>
2021-08-20 10:39:35 +0800

    Update and rename to_b_connect.py to device_control_and_mq.py

--- example/device_control_and_mq.py
+++ example/device_control_and_mq.py
@@ -1,6 +1,5 @@
-"""This module has components that are used for testing to B connect."""
+"""This module has components that are used for testing tuya's device control and Pulsar massage queue."""
 import logging
-from tuya_connector.tuya_enums import TuyaCloudPulsarTopic
 from tuya_connector import (
     TuyaOpenAPI,
     TuyaOpenPulsar,
@@ -8,20 +7,24 @@ from tuya_connector import (
     TUYA_LOGGER,
 )
 
-ACCESS_ID = "xxxxx"
-ACCESS_KEY = "xxxxx"
+ACCESS_ID = "your-access-id"
+ACCESS_KEY = "your-access-key"
+API_ENDPOINT = "https://openapi.tuyacn.com"
+MQ_ENDPOINT = "wss://mqe.tuyacn.com:8285/"
 
 # Enable debug log
 TUYA_LOGGER.setLevel(logging.DEBUG)
 
 # Init openapi and connect
-openapi = TuyaOpenAPI("https://openapi.tuyacn.com", ACCESS_ID, ACCESS_KEY)
+openapi = TuyaOpenAPI(API_ENDPOINT, ACCESS_ID, ACCESS_KEY)
 openapi.connect()
+
+# Call any API from Tuya
 response = openapi.get("/v1.0/statistics-datas-survey", dict())
 
 # Init Message Queue
 open_pulsar = TuyaOpenPulsar(
-    ACCESS_ID, ACCESS_KEY, "wss://mqe.tuyacn.com:8285/", TuyaCloudPulsarTopic.TEST
+    ACCESS_ID, ACCESS_KEY, MQ_ENDPOINT, TuyaCloudPulsarTopic.TEST
 )
 # Add Message Queue listener
 open_pulsar.add_message_listener(lambda msg: print(f"---\nexample receive: {msg}"))
@@ -32,3 +35,4 @@ open_pulsar.start()
 input()
 # Stop Message Queue
 open_pulsar.stop()
+
